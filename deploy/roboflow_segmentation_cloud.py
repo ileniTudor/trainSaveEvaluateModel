38f811c6e0b13d833243cb7a043090e8e6a78a61
tudor <tudor.ileni@tvarita.ro>
2024-11-17 23:14:35 +0200

    update roboflow segmentation

---
+++ deploy/roboflow_segmentation_cloud.py
@@ -0,0 +1,9 @@
+from roboflow import Roboflow
+rf = Roboflow(api_key="your key")
+
+project = rf.workspace().project("dentalxray-s3wqb-5bdef")
+model = project.version(1).model
+
+# print(model.predict("your_image.jpg", confidence=40, overlap=30).json())
+
+model.predict("180_2247.jpg").save("roboflow_prediction.jpg")
